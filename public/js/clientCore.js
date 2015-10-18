d5fa0cd2661180867440bfb9c2d976c79f1f84e0
Jose Velasco <root@Jose-LPC>
2015-10-18 20:37:33 +0200

    bola funcionando

---
+++ public/js/clientCore.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+
+    var socket = io();
+    // Test comunicación
+    socket.on('connect', function(){
+        console.log('Hello Server');
+    });
+
+    //CANVAS GAME
+    var canvas = CE.defines("canvas").
+        extend(Animation).
+        extend(Input).
+        extend(Hit).
+        ready(function() {
+            canvas.Scene.call("MyScene",{
+            });
+
+        });
+
+    //DEFINICION DEL JUEGO
+    canvas.Scene.New({
+
+        //Nombre
+        name: "MyScene", // Obligatory
+
+        //Texturas
+        materials: {
+            images: {
+                background: "/images/background.jpg",    //Stage
+                bola: "/images/bola.gif"
+            }
+        },
+
+        //METODOS
+        ready: function(stage, params) {            //PREPARA LA INTERFAZ
+
+            //STAGE
+            this.dimensions = this.getCanvas();
+            var scene = canvas.Scene.get("MyScene");
+
+            //escenario
+            this.background = this.createElement();
+            this.background.drawImage("background");
+            stage.append(this.background);
+
+            //NUEVO JUGADOR
+            this.bola = this.createElement();
+            this.bola.drawImage("bola");
+            this.bola.scaleTo(0.15);
+            socket.emit("NewPlayer", socket.id); //EMITE QUE SE HA CONECTADO
+
+            //MOVIMIENTOS
+            canvas.Input.keyDown(Input.Left, function() {
+                console.log("aaa iz");
+                socket.emit("ClientsMove", "KEYLEFT");
+            });
+            canvas.Input.keyDown(Input.Right, function() {
+                socket.emit("ClientsMove", "KEYRIGHT");
+            });
+            canvas.Input.keyDown(Input.Up, function() {
+                socket.emit("ClientsMove", "KEYUP");
+            });
+            canvas.Input.keyDown(Input.Bottom, function() {
+                socket.emit("ClientsMove", "KEYDOWN");
+            });
+
+            socket.on("BallPosition", function(currentX, currentY){
+                scene.bola.x = currentX;
+                scene.bola.y= currentY;
+                stage.append(scene.bola);
+            });
+
+        },
+
+        render: function(stage){
+            stage.refresh();
+        }
+    });
+});
---
+++ routes/io.js
@@ -0,0 +1,45 @@
+
+var io = require('socket.io')();
+Ball = require("./Ball").Ball;
+
+ball = new Ball(0,0);
+
+
+
+
+io.sockets.on('connection', function(socket){
+    // Test comunicación
+    socket.on('connection', function(socket){
+        console.log('Hello Clients');
+        console.log(bola.x);
+    });
+
+    socket.on('NewPlayer', function(clientID){
+        console.log("NUEVO JUGADOR. LISTA ACTUALIZADA");
+        console.log(clientID);
+        console.log("Posicion de la bola para el nuevo jugador "+ ball.getX() +" - "+ ball.getY());
+        io.emit("BallPosition", ball.getX(),ball.getY());
+
+    });
+
+    //MOVIMIENTO
+    socket.on("ClientsMove", function(motion){
+        if(motion == "KEYUP"){
+            ball.setY(ball.getY() - 25);
+        }
+        else if(motion == "KEYDOWN"){
+            ball.setY(ball.getY() + 25);
+        }
+        else if(motion == "KEYLEFT"){
+            ball.setX(ball.getX() - 25);
+        }
+        else if(motion == "KEYRIGHT"){
+            ball.setX(ball.getX() + 25);
+        }
+        io.emit("BallPosition", ball.getX(),ball.getY());
+
+    });
+
+});
+
+module.exports = io;
